3ace66173a5b188e870342986ae1d11c7ca272de
Ana Luiza Ferrer <analcferrer@gmail.com>
2018-12-02 23:29:02 -0800

    destinations view controller

--- Safar/DestinationsViewController.swift
+++ Safar/DestinationsViewController.swift
@@ -6,15 +6,28 @@
 //  Copyright © 2018 Safar. All rights reserved.
 //
 
-import Foundation
 import UIKit
 
-class DestinationsViewController: UIViewController {
+class DestinationsViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+
+    @IBOutlet weak var destinationsCollectionView: UICollectionView!
     
     override func viewDidLoad() {
-        
-        
-        
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
     }
     
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    
+         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "destinationCollectionViewCell", for: indexPath) as! DestinationCollectionViewCell
+        
+        return cell
+        
+    }
+
 }
